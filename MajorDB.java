08864ce9f36463d8b52dc7db353528eada1b9d9e
ytakayanagi <yujintakayanagi@gmail.com>
2016-11-09 19:27:59 -0800

    Update MajorDB.java

--- MajorDB.java
+++ MajorDB.java
@@ -1,3 +1,23 @@
+import java.util.Hashtable;
+
 class MajorDB {
 
+    private Hashtable<Integer, Major> major;
+    
+    public Major search(Integer id) {
+        return major.get(id);
+    }
+    
+    public void add(Integer id, Major m) {
+        courses.put(id, m);
+    }
+    
+    public void delete(Integer id, Major m) {
+        courses.remove(id, m);
+    }
+    
+    public Hashtable<Integer, Major> export() {
+        return major;
+    }
+    
 }
