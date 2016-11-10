ce879e9568b0f1297f455cda00111476162d3535
Ryan Vitacion <ryan.vitacion.108@my.csun.edu>
2016-11-09 17:25:06 -0800

    Rvitacion (#2)
    * Update CourseDB.java
    
    * Update CourseDB.java
    
    * Update CourseDB.java
    
    * Update CourseDB.java
    
    * Update CourseDB.java

--- CourseDB.java
+++ CourseDB.java
@@ -1,3 +1,23 @@
+import java.util.Hashtable;
+
 class CourseDB {
 
+    private Hashtable<Integer, Course> courses;
+    
+    public Course search(Integer id) {
+        return courses.get(id);
+    }
+    
+    public void add(Integer id, Course crs) {
+        courses.put(id, crs);
+    }
+    
+    public void delete(Integer id) {
+        courses.remove(id);
+    }
+    
+    public Hashtable<Integer, Course> export() {
+        return courses;
+    }
+    
 }
