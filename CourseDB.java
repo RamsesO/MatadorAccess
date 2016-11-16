0ccd81fc948dc6814b530450e219adaaf5b8a46d
Ryan Vitacion <ryan.vitacion.108@my.csun.edu>
2016-11-16 14:40:56 -0800

    Updated CourseDB to support search by String

--- CourseDB.java
+++ CourseDB.java
@@ -1,19 +1,31 @@
 import java.util.Hashtable;
 
 class CourseDB {
-
+    
+    private Hashtable<String, Integer> stringTable;
     private Hashtable<Integer, Course> courses;
     
     public Course search(Integer id) {
         return courses.get(id);
     }
     
-    public void add(Integer id, Course crs) {
-        courses.put(id, crs);
+    public Course search(String name) {
+        return courses.get(stringTable.get(name));
+    }
+    
+    public void add(Course crs) {
+        courses.put(crs.getCourseNum(), crs);
+        stringTable.put(crs.getName(), crs.getCourseNum());
     }
     
     public void delete(Integer id) {
+        stringTable.remove(courses.get(id).getCourseNum());
         courses.remove(id);
+    }
+    
+    public void delete(String name) {
+        courses.remove(stringTable.get(name).getCourseNum());
+        stringTable.remove(name);
     }
     
     public Hashtable<Integer, Course> export() {
