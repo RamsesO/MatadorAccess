e8e65c56974420d5f2d3a91160c6299741ae01cd
Ryan Vitacion <ryan.vitacion.108@my.csun.edu>
2016-11-16 15:46:33 -0800

    Upload login to master branch

--- Login.java
+++ Login.java
@@ -1,5 +1,37 @@
-// It's mine!
+import java.util.Scanner;
 
-{
+public class login {
+
+  public void authenticate() {
+    
+    String username, password, inputUsername, inputPassword;
+    Scanner scan = new Scanner(new File(""));
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter your username and password.");
+    
+    // Prompt user to enter username
+    System.out.println("Username: ");
+    inputUsername = input.nextLine();
+    
+    // Prompt user to enter password
+    System.out.println("Password: ");
+    inputPassword input.nextLine();
+    
+    while(scan.hasNextInt()) {
+    
+      String username = scan.nextLine();
+      String password = scan.nextLine();
+      
+      if(inputUsername.equals(username) && inputPassword.equals(password)) {
+        success();
+      }
+      
+    }
+    
+    fail();
+    
+  }
+  
 
 }
