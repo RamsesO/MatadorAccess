6ecbdd25baf8470e885a8bbde28d97b1b1b9c92f
RamsesO <ordonez.ramses@gmail.com>
2016-11-21 16:08:57 -0800

    serializable

--- ProfileDB.java
+++ ProfileDB.java
@@ -1,9 +1,4 @@
 import java.util.Hashtable;
-import java.lang.File;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.Serializable;
 
 
