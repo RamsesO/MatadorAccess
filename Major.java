154f53fac7d5080a9df47f3782956711ce9f13ce
HMojib <hamounmojib@gmail.com>
2016-12-04 21:44:12 -0800

    Added motherfucking comments to this motherfucking object

--- Major.java
+++ Major.java
@@ -1,10 +1,33 @@
+/*---------------------------------------------------------------------------
+Module Name: Major (Creates Major object with various functions )
+Inputs: ArrayLists; Assorted variable types representing Major attributes
+Outputs: Assorted types: strings, integers, doubles, ArrayLists
+Submodules: N/A
 
+Author: Hamoun Mojib
+Date: 11/9/16
+
+Reviewer: Ryan Vitacion
+Date: 11/27/16
+
+- Revision History -
+Programmer: Ryan Vitacion
+Date: 11/27/16
+Description of Changes: Made class serializable
+Reviewer:
+Date of Review:
+
+Programmer: Hamoun Mojib
+Date: 12/4/16
+Description of Changes: Comments
+Reviewer:
+---------------------------------------------------------------------------*/
 import java.io.*;
 import java.util.*;
 
 public class Major implements Serializable {
 
-    // Main variables
+    // Main variables to use in Major object
     private int majorId;
     private int averageTime;
     private int averageSalary;
@@ -17,7 +40,8 @@ public class Major implements Serializable {
     private String description;
     private ArrayList<String> relatedJobs;
     private ArrayList<String> courses;
-
+    
+    //Major object constructor
     public Major() {
         this.majorId = 0;
         this.averageTime = 0;
@@ -32,7 +56,8 @@ public class Major implements Serializable {
         this.relatedJobs = null;
         this.courses = null;
     }
-
+    
+    //Updates Major object
     public Major(int majorId, int averageTime, int averageSalary,
             int numberOfStudents, int maleStudents, int femaleStudents,
             double difficultyRating, String majorName, String genderRatio,
@@ -51,75 +76,93 @@ public class Major implements Serializable {
         this.relatedJobs = relatedJobs;
         this.courses = courses;
     }
-
+    
+    //Getter for Major ID number
     public int getMajorId() {
         return majorId;
     }
-
+    
+    //Setter for Major ID number
     public void setMajorId(int majorId) {
         this.majorId = majorId;
     }
-
+    
+    //Getter for Major's average time to graduation 
     public int getAverageTime() {
         return averageTime;
     }
 
+    //Setter for Major's average time to graduation
     public void setAverageTime(int averageTime) {
         this.averageTime = averageTime;
     }
-
+    
+    //Getter for Major's average salary
     public int getAverageSalary() {
         return averageSalary;
     }
-
+    
+    //Setter for Major's average Salary
     public void setAverageSalary(int averageSalary) {
         this.averageSalary = averageSalary;
     }
-
-        public int getNumberOfStudents() {
+    
+    //Getter for current number of students in major
+    public int getNumberOfStudents() {
         return numberOfStudents;
     }
-
+    
+    //Setter for number of students in Major
     public void setNumberOfStudents(int numberOfStudents) {
         this.numberOfStudents = numberOfStudents;
     }
     
+    //Getter for number of Male students in Major
     public int getMaleStudents() {
         return maleStudents;
     }
-
+    
+    //Setter for number of Male students in Major
     public void setMaleStudents(int maleStudents) {
         this.maleStudents = maleStudents;
     }
-
+    
+    //Getter for number of Female students in Major
     public int getFemaleStudents() {
         return femaleStudents;
     }
-
+    
+    //Setter for number of Female students in Major
     public void setFemaleStudents(int femaleStudents) {
         this.femaleStudents = femaleStudents;
     }
-
+    
+    //Getter for difficulty rating of Major
     public double getDifficultyRating() {
         return difficultyRating;
     }
-
+    
+    //Setter for difficulty rating of Major 
     public void setDifficultyRating(double difficultyRating) {
         this.difficultyRating = difficultyRating;
     }
-
+    
+    //Getter for name of Major
     public String getMajorName() {
         return majorName;
     }
-
+    
+    //Setter for name of Major
     public void setMajorName(String majorName) {
         this.majorName = majorName;
     }
-
+    
+    //Getter for gender ratio of Major
     public String getGenderRatio() {
         return genderRatio;
     }
-
+    
+    //Setter for gender ratio of Major
     public void setGenderRatio(int maleStudents, int femaleStudents) {
         int gcd = 0;
         if (maleStudents > femaleStudents) {
@@ -135,33 +178,42 @@ public class Major implements Serializable {
 
     }
     
+    //Getter for Description of Major
     public String getDescription() {
         return description;
     }
-
+    
+    //Setter for Description of Major
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
+    //Getter for related jobs with degree in Major
     public ArrayList<String> getRelatedJobs() {
         return relatedJobs;
     }
-
+    
+    //Setter for related jobs with degree in Major
     public void setRelatedJobs(ArrayList<String> relatedJobs) {
         this.relatedJobs = relatedJobs;
     }
-
+    
+    //Getter for courses in Major
     public ArrayList<String> getCourses() {
         return courses;
     }
-
+    
+    //Setter for courses in Major
     public void setCourses(ArrayList<String> courses) {
         this.courses = courses;
     }    
-
+    
+    //Supporting function to setGenderRatio. Calculates GCD between two integers
     private int GCD(int smallerInt, int largerInt) {
         return largerInt == 0 ? smallerInt : GCD(largerInt, smallerInt % largerInt);
     }
+    
+    //toString method for Major object
     @Override
     public String toString() {
     	String str = "Major: " + this.majorName + "\n" +
